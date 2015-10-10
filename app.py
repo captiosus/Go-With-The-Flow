a755d52a3970748967933967d63b1da6cdfc13b2
captiosus <royxu652@gmail.com>
2015-10-10 16:16:06 -0400

    token: git commit -a -mtoken:

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
-from flask import Flask, render_template, request, session
-import utils
+from flask import Flask, render_template, request, session, redirect, url_for, session
+import utils, time
 
 app = Flask(__name__)
-
 
 @app.route("/")
 def index():
@@ -27,6 +26,9 @@ def login():
         uname = request.form['username']
         pword = request.form['password']
         if utils.authenticate(uname, pword) == 1:
+            utils.sess = utils.genToken();
+            session['token'] = utils.sess;
+            utils.logtime = time.gmtime()
             return redirect(url_for('calender'))
         else:
             return render_template("login.html", err=err)
@@ -39,10 +41,16 @@ def logout():
 
 @app.route("/calender")
 def calender():
-  if verify():
-    return render_template("calender.html")
+    if 'token' not in session:
+        return render_template("login.html")
+    else:
+        if session['token'] == utils.sess:
+            if (time.gmtime())[3] - (utils.logtime)[3] < 1 and (time.gmtime())[2] == (utils.logtime)[2] and (time.gmtime())[1] == (utils.logtime)[1] and (time.gmtime())[0] == (utils.logtime)[0]:
+                return render_template("calender.html")
+    return redirect(url_for('login'))
 
 
 if __name__ == "__main__":
     app.debug = True
+    app.secret_key="david veller"
     app.run(host="0.0.0.0", port=8000)
--- utils.py
+++ utils.py
@@ -1,7 +1,11 @@
 import sqlite3
 import csv
 import hashlib
+import unicodedata
+import random
 
+sess = ""
+logtime = ""
 #def create():
 #    conn = sqlite3.connect("login.db")
 #    c = conn.cursor()
@@ -11,10 +15,13 @@ import hashlib
 def newUser(username, password):
     conn = sqlite3.connect("login.db")
     c = conn.cursor()
-    isTaken = c.execute("SELECT EXISTS(SELECT 1 FROM login WHERE user=:uname LIMIT 1)", {"uname":username})
-    if isTaken == 0:
+    isTaken = c.execute("SELECT user FROM login WHERE user=:uname LIMIT 1", {"uname":username})
+    data = c.fetchone()
+    if data is None :
         m = hashlib.sha224(password)
-        c.execute("INSERT INTO login VALUES(?, ?)", {username, m.hexdigest()})
+        query = "INSERT INTO login VALUES (\"%s\", \"%s\")" % (username, m.hexdigest())
+        c.execute(query)
+        conn.commit()
         return 1
     return 0
 
@@ -22,9 +29,21 @@ def newUser(username, password):
 def authenticate(username, password):
     conn = sqlite3.connect("login.db")
     c = conn.cursor()
-    m = hashlib.sha224(password)
-    c = c.execute("SELECT pass FROM login WHERE user=:uname", {"uname":username})
-    if c == m:
+    m = hashlib.sha224(password).hexdigest()
+    query = "SELECT pass FROM login WHERE user=\"%s\"" % (username)
+    c.execute(query)
+    s1 = c.fetchone()
+    if s1 == None:
+        return 0
+    s2 = s1[0]
+    if s2 == m:
+        print "hello"
         return 1
 
     return 0
+
+def genToken():
+    token = ""
+    for x in range(64):
+        token += str(random.randrange(0, 9))
+    return token
